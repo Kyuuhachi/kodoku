134f9b7091263e586e31d3e0795155c80fa6d34d
Kyuuhachi <caagr98@gmail.com>
2022-04-05 17:20:21 +0200

    Make /usr/lib* not have their own homes
    Unfortunately firefox is in /usr/lib/firefox/firefox, so I had to special-case that

--- kodoku.c
+++ kodoku.c
@@ -7,6 +7,7 @@
 #include <dlfcn.h>
 #include <sys/auxv.h>
 #include <assert.h>
+#include <stdbool.h>
 
 #define DLLEXPORT __attribute__((__visibility__("default")))
 DLLEXPORT int execl(const char *pathname, const char *arg, ... /*, (char *) NULL */);
@@ -22,16 +23,6 @@ DLLEXPORT int execve(const char *file, char *const argv[], char *const envp[]);
 static char *ko_home_self = NULL;
 static char *ko_home_misc = NULL;
 
-void ko_set_home(char **envp, char *home) {
-	if(home == NULL) return;
-	assert(strncmp(home, "HOME=", 5) == 0);
-	for(int i = 0; envp[i]; i++)
-		if(strncmp(envp[i], "HOME=", 5) == 0) {
-			envp[i] = home;
-			return;
-		}
-}
-
 char *ko_build_home(char *dir, char *suf) {
 	char *str = malloc(5 + strlen(dir) + 1 + strlen(suf) + 1);
 	strcpy(str, "HOME=");
@@ -44,19 +35,30 @@ char *ko_build_home(char *dir, char *suf) {
 __attribute__((constructor)) void ko_init() {
 	char *dir = getenv("KODOKU_HOME");
 	if(dir) {
-		char *suf = getenv("KODOKU_MISC");
-		if(suf == NULL) suf = "misc";
-		ko_home_misc = ko_build_home(dir, suf);
-
 		char *exe = (char*)getauxval(AT_EXECFN);
 		char buf[PATH_MAX+1];
-		if((exe != NULL && realpath(exe, buf) != NULL) || realpath("/proc/self/exe", buf) != NULL) {
-			char *last = strrchr(buf, '/');
-			if(last != NULL) {
-				ko_home_self = ko_build_home(dir, last+1);
-				ko_set_home(environ, ko_home_self);
+		if((exe != NULL && realpath(exe, buf) != NULL)
+		|| realpath("/proc/self/exe", buf) != NULL) {
+			bool is_lib = false;
+			is_lib |= strncmp(buf, "/usr/lib/", 9) == 0;
+			is_lib |= strncmp(buf, "/usr/lib32/", 11) == 0;
+			is_lib |= strncmp(buf, "/usr/lib64/", 11) == 0;
+			is_lib &= strncmp(buf, "/usr/lib/firefox/", 16) != 0;
+			if(!is_lib) {
+				char *last = strrchr(buf, '/');
+				assert(last != NULL);
+				setenv("KODOKU_NAME", last+1, 1);
 			}
 		}
+
+		char *misc = getenv("KODOKU_MISC");
+		if(misc == NULL) misc = "misc";
+		if(!getenv("KODOKU_NAME")) setenv("KODOKU_NAME", misc, 1);
+		char *name = getenv("KODOKU_NAME");
+
+		ko_home_self = ko_build_home(dir, name);
+		ko_home_misc = ko_build_home(dir, misc);
+		putenv(ko_home_self);
 	}
 }
 
@@ -65,17 +67,22 @@ __attribute__((destructor)) void ko_deinit() {
 	free(ko_home_misc);
 }
 
+int o_execve(const char *file, char *const argv[], char *const envp[]) {
+	static __typeof(o_execve) *super;
+	if(!super) super = dlsym(RTLD_NEXT, "execve");
+	return super(file, argv, envp);
+}
+
 int execve(const char *file, char *const argv[], char *const envp[]) {
-	static __typeof(execve) *o_execve;
-	if(!o_execve) o_execve = dlsym(RTLD_NEXT, "execve");
-
-	if(envp == NULL)
-		return o_execve(file, argv, envp);
-
+	if(envp == NULL) envp = environ;
 	int i;
 	for(i = 0; envp[i] != 0; i++) {}
 	char *new_envp[i+1];
 	memcpy(new_envp, envp, sizeof(new_envp));
-	ko_set_home(new_envp, ko_home_misc);
+	for(int i = 0; new_envp[i]; i++)
+		if(strncmp(new_envp[i], "HOME=", 5) == 0) {
+			new_envp[i] = ko_home_misc;
+			break;
+		}
 	return o_execve(file, argv, new_envp);
 }
